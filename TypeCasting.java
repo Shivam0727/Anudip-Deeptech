9bf5ef3cc01ac6a7f81154cf718b29fef813e2c9
Shivam0727 <108928939+Shivam0727@users.noreply.github.com>
2022-07-14 15:28:46 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,16 @@
+
+class TypeCasting {
+    public static void main(String[] args)
+    {
+            
+        char ch1 = 'B';  
+	char ch2 = 'C';
+	char ch3 = 'D';         
+        byte x = (byte) ch1 ;  
+	byte y = (byte) ch2 ;
+	byte z = (byte) ch3 ;       
+        System.out.println(x);
+	System.out.println(y);
+	System.out.println(z);
+    }
+}
