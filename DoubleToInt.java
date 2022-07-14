6bb81c1afcd9c07a68bfd52bde3bbc1012825e06
Shivam0727 <108928939+Shivam0727@users.noreply.github.com>
2022-07-14 16:57:17 +0530

    Add files via upload

---
+++ DoubleToInt.java
@@ -0,0 +1,18 @@
+public class DoubleToInt
+ {
+	public static void main(String[] args)	
+	 {
+		double myDouble1 = 10.46;
+		double myDouble2 = 7.89;
+		int myInt1 = (int) myDouble1;
+		int myInt2 = (int) myDouble2;
+		System.out.print("Enter your double value : ");
+		System.out.println(myDouble1);
+		System.out.print("After converting the value is : ");
+		System.out.println(myInt1);
+		System.out.print("Enter your double value : ");
+		System.out.println(myDouble2);
+		System.out.print("After converting the value is : ");
+		System.out.println(myInt2);
+         }
+ }
