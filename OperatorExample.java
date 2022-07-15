6aa73b70303cf683c5c795984bea79d3ca76ef5c
Shivam0727 <108928939+Shivam0727@users.noreply.github.com>
2022-07-15 17:44:32 +0530

    Add files via upload

---
+++ OperatorExample.java
@@ -0,0 +1,16 @@
+class OperatorExample
+{
+	public static void main(String args[])
+	{
+		int a=18;
+		int b=7;
+                System.out.print("The addition of two operands are : ");
+		System.out.println(a+b);
+		System.out.print("The Subtraction of two operands are : "); 
+		System.out.println(a-b); 
+		System.out.print("The Multiplication of two operands are : ");
+		System.out.println(a*b); 
+		System.out.print("The Division of two operands are : ");
+		System.out.println(a/b);
+	}
+}
