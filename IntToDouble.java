9922433793b628981fe90e887d4ea8f9683a3e5e
Shivam0727 <108928939+Shivam0727@users.noreply.github.com>
2022-07-15 11:36:45 +0530

    Add files via upload

---
+++ IntToDouble.java
@@ -0,0 +1,18 @@
+class IntToDouble
+ {
+	public static void main(String[] args)	
+	 {
+		int myInt1 = 10;
+		int myInt2= 7;
+		double myDouble1 =  myInt1;
+		double myDouble2 =  myInt2;
+		System.out.print("Enter your double value : ");
+		System.out.println(myInt1);
+		System.out.print("After converting the value is : ");
+		System.out.println(myDouble1);
+		System.out.print("Enter your double value : ");
+		System.out.println(myInt2);
+		System.out.print("After converting the value is : ");
+		System.out.println(myDouble2);
+         }
+ }
