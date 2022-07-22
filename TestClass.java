cb464dcd9dcbdf2b7686b455e72e69138dc8d8ca
Shivam0727 <108928939+Shivam0727@users.noreply.github.com>
2022-07-22 16:29:08 +0530

    Add files via upload

---
+++ TestClass.java
@@ -0,0 +1,9 @@
+public class TestClass
+	 {
+		int y = 8;
+		public static void main(String[] args)
+			{
+				TestClass myObj = new TestClass();
+				System.out.println(myObj.y);
+			}
+	}
---
+++ TestClass1.java
@@ -0,0 +1,9 @@
+public class TestClass1
+	 {
+		int z = 7;
+		public static void main(String[] args)
+			{
+				TestClass1 myObj = new TestClass1();
+				System.out.println(myObj.z);
+			}
+	}
